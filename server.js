b5770d2a98bae35db7e78ade1c0790d5d48ed229
hpshalom <hpshalom@icloud.com>
2025-04-03 18:22:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const braintree = require('braintree');
-require('dotenv').config();
-
 const app = express();
-app.use(cors());
+const port = process.env.PORT || 3000;
 
+// Braintree credentials
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY
+  merchantId: '6mhvv2wg87mmsdcd',
+  publicKey: 'hfhgyc349hzqgb85',
+  privateKey: 'ac450f83c78be02e5538ef870d41c3c0',
 });
 
-app.get('/client-token', async (req, res) => {
+// Serve static files (including index.html)
+app.use(express.static(path.join(__dirname, '/')));
+
+// Endpoint to get client token
+app.get('/client_token', async (req, res) => {
   try {
     const response = await gateway.clientToken.generate({});
-    res.send({ clientToken: response.clientToken });
+    res.send(response.clientToken);
   } catch (err) {
-    console.error(err);
     res.status(500).send('Error generating client token');
   }
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Donation server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
